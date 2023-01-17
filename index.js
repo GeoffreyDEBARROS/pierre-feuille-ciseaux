ca6bf29d662d015e6461d6b70b6752fd2dc9d3b3
DE BARROS Geoffrey <debarrosg01@gmail.com>
2023-01-17 19:42:35 +0100

    Base du JS terminer

---
+++ index.js
@@ -0,0 +1,62 @@
+const playerChoice = document.querySelector(".player-choice");
+const randomChoice = document.querySelector(".random-choice");
+
+// PLayer Buttons //
+let choices = ["pierre", "feuille", "ciseaux"];
+let player = "";
+const imgRock = document.createElement("img");
+const imgPaper = document.createElement("img");
+const imgScissors = document.createElement("img");
+const imgRandom = document.createElement("img");
+imgRock.src = "./img/pierre.jpg";
+imgPaper.src = "./img/feuille.jpg";
+imgScissors.src = "./img/ciseaux.jpg";
+
+
+rock.addEventListener("click", () => {
+  imgRock.height = "200";
+  playerChoice.innerHTML = "";
+  playerChoice.appendChild(imgRock);
+  player = "rock";
+
+  randomChoice.innerHTML = "";
+  random = Math.floor(Math.random() * 3);
+  imgRandom.src = `./img/${choices[random]}.jpg`;
+  imgRandom.height = "200";
+  randomChoice.appendChild(imgRandom);
+  setTimeout(() => {
+    randomChoice.innerHTML = ""; 
+  }, 1500);
+});
+
+paper.addEventListener("click", () => {
+  imgPaper.height = "200";
+  playerChoice.innerHTML = "";
+  playerChoice.appendChild(imgPaper);
+  player = "paper";
+
+  random = Math.floor(Math.random() * 3);
+  imgRandom.src = `./img/${choices[random]}.jpg`;
+  randomChoice.innerHTML = "";
+  imgRandom.height = "200";
+  randomChoice.appendChild(imgRandom);
+  setTimeout(() => {
+    randomChoice.innerHTML = ""; 
+  }, 1500);
+});
+
+scissors.addEventListener("click", () => {
+  imgScissors.height = "200";
+  playerChoice.innerHTML = "";
+  playerChoice.appendChild(imgScissors);
+  player = "scissors";
+
+  random = Math.floor(Math.random() * 3);
+  imgRandom.src = `./img/${choices[random]}.jpg`;
+  randomChoice.innerHTML = "";
+  imgRandom.height = "200";
+  randomChoice.appendChild(imgRandom);
+  setTimeout(() => {
+    randomChoice.innerHTML = ""; 
+  }, 1500);
+});
